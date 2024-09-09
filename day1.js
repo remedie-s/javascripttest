27d97c5fe55333d7efedc5d143b7ebc18195e1f2
remedie-s <tree1808@gmail.com>
2024-09-09 14:23:22 +0900

    0909 1423

--- day1.js
+++ day1.js
@@ -14,8 +14,8 @@
 //  let : 블록 스코프 변수
 //  const : 상수
 
-const PI =3.14;
-console.log(PI);
+// const PI =3.14;
+// console.log(PI);
 // PI=6666;
 
 console.log('\n\n');
@@ -33,8 +33,8 @@ function test(){
     //에러가 발생함, 영역을 벗어남
     // console.log('c=',c);
 }
-test();
-console.log('함수 밖에서 a=',a);
+// test();
+// console.log('함수 밖에서 a=',a);
 //  ReferenceError: b is not defined
 // console.log('함수 밖에서 b=',b);
 
@@ -50,11 +50,11 @@ function test2(){
     // let name1 = 'B';
     console.log(name1);
 }
-test2();
+// test2();
 
 let a1=1, b1=2, c1;
 // 1 2 undefined 선언안되면 x
-console.log(a1,b1,c1);
+// console.log(a1,b1,c1);
 
 // ES6 에서는 let, const사용을 적극권장
 // 실제 구동시 통상 대부분 프레임웤에서는 var로 변환되서 처리
@@ -66,4 +66,55 @@ console.log(a1,b1,c1);
 const noNameFunc=function (name='자바',year=2024){
     console.log(name=`${name}`, year=`${year} 입니다.`);
 }
-noNameFunc();
+// noNameFunc();
+
+// 특정 함수의 안자값으로 익명함수 사용
+function noName(){
+    setTimeout(function(){
+        console.log('->')
+    },1000);
+}
+// noName();
+
+function noName1(){
+    setTimeout(function(){
+        console.log('1');
+    },2000);
+}
+// noName1();
+
+// 화살표 함수 
+// 표준함수 => 익명함수 변경절차
+// 1. function 제거
+// 2. 파라미터가 1개이면 () 제거할 수 있다. 반면 없으면 ()사용
+// 3. () {} 사이에 => 추가
+// 4. {} 는 수행문이 하나인 경우 생략가능
+// 5. 수행문이 하나이고, 중괄호가 생략되어 있으면 return 생략
+
+function getInfoArrow(name) {
+    console.log(`${name} ...!!!@@@`);
+}
+// getInfoArrow('sejong1');
+
+//화살표 함수로 변경
+const getInfoArrow1=function(name){
+    console.log(`${name} ...!!!@@@`)
+}
+// getInfoArrow1('sejong2');
+
+const getInfoArrow2= name => console.log(`${name} ...!!!@@@`);
+// getInfoArrow2('sejong3');
+
+// 파라미터가 2개 이상인 경우의 화살표 함수
+function getInfoArrow3(name,age){
+    return `${name} ${age} ....!@!@`;
+}
+console.log(getInfoArrow3('월요일',30));
+
+const getInfoArrow4=function(name,age){
+    return `${name} ${age} ....!@!@`;
+}
+console.log(getInfoArrow4('화요일',40));
+
+const getInfoArrow5=( name,age ) => `${name} ${age} ....!@!@`;
+console.log(getInfoArrow5('수요일',50));
