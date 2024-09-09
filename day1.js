0cb5a21cd58033c931feb4921d0f6454b908e45f
remedie-s <tree1808@gmail.com>
2024-09-09 11:40:40 +0900

    Feat 0909 자바 스크립트 시작

---
+++ day1.js
@@ -0,0 +1,9 @@
+//표준함수
+// function full(name, year){
+//     console.log(name, year);
+// }
+// 문자열 표시 "",''
+function full(name='JAVA',year=2024){
+    console.log(name='${name},year=${year} 입니다');
+}
+full('자바',2024);
