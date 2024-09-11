dd2db78cea069eea5f8f5e320bdcae120e358a2d
remedie-s <tree1808@gmail.com>
2024-09-11 16:17:40 +0900

    Fix: 0911 오후 수업 끝

--- test/test/src/main/java/com/example/test/test/TestController.java
+++ test/test/src/main/java/com/example/test/test/TestController.java
@@ -1,14 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 package com.example.test.test;
 
-/**
- *
- * @author user
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class TestController {
 
+    @GetMapping("/")
+    public String test() {
+        return "ajaxTest";
+    }
+
+    @PostMapping("/json")
+    @ResponseBody
+    public List<TestDTO> getJson(@RequestParam("today") String today, @RequestParam("human") String human) {
+        System.out.println(today + "!!!1");
+        System.out.println(human + "!!!2");
+        ArrayList<TestDTO> al = new ArrayList<>();
+        TestDTO td = new TestDTO();
+        td.setId("asdf");
+        td.setTestNum(1);
+        al.add(td);
+        TestDTO td1 = new TestDTO();
+        td1.setId("asdfsdf");
+        td1.setTestNum(2);
+        al.add(td1);
+        return al;
+    }
 }
---
+++ test/test/src/main/java/com/example/test/test/TestDTO.java
@@ -0,0 +1,12 @@
+package com.example.test.test;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TestDTO {
+    String id;
+    Integer testNum;
+
+}
